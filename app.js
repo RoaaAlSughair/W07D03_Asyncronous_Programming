a1c3967583fb31a9c3d6f12d778fcf15f846c96d
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-05-12 00:26:57 +0300

    Pulse check: Question 2

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ const axios = require("axios");
 
 const app = express();
 app.use(express.json())
-const PORT = 3000;
 
+const readFiles = ("data.txt", (data, error) => {
+    if (error) {
+        throw error;
+    };
+    console.log(data.toString());
+});
+
+
+
+const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`The server is listening at port: ${PORT}`);
+    console.log(`The server is listening at http://localhost:${PORT}`);
 })
