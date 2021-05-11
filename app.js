c09d5eb41f097f202f671bebf4bbab58e7225f01
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-05-12 01:18:17 +0300

    Pulse check: Question 4

--- app.js
+++ app.js
@@ -3,33 +3,46 @@ const fs = require("fs");
 const axios = require("axios");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-const readFiles = (() => {
-    fs.readFile("data.txt", (data, error) => {
-        if (error) {
-            throw error;
-        };
-        console.log(data.toString());
-    })
-});
+const readFiles = () => {
+  fs.readFile("./data.txt", (error, data) => {
+    if (error) {
+      throw error;
+    }
+    console.log(data.toString());
+  });
+};
 
 readFiles();
 
 const writeFiles = () => {
-    fs.writeFile("text.txt", "A new file has been created", (error) => {
-        if (error) {
-            throw error;
-        }
-        console.log("Created successfully");
-    });
+  fs.writeFile("./text.txt", "A new file has been created", (error) => {
+    if (error) {
+      throw error;
+    }
+    console.log("Created successfully");
+  });
 };
 
 writeFiles();
+
+const getPost = (id) => {
+  axios
+    .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+getPost(1);
 
 
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`The server is listening at http://localhost:${PORT}`);
-})
+  console.log(`The server is listening at http://localhost:${PORT}`);
+});
